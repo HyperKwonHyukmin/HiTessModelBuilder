76fe3d1cb0cd0c706b9edab99672bd730949a9c1
HyperKwonHyukmin <khm7529@gmail.com>
2026-02-13 15:34:38 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using HiTessModelBuilder.Services.Initialzation;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HiTessModelBuilder
 {
@@ -6,7 +8,14 @@ namespace HiTessModelBuilder
   {
     static void Main(string[] args)
     {
+      string StrucCsv = PathManager.Current.Stru;
+      string PipeCsv = PathManager.Current.Pipe;
+      string EquipCsv = PathManager.Current.Equip;
 
+      string CsvFolderPath = Path.GetDirectoryName(StrucCsv);
+      string inputFileName = Path.GetFileName(StrucCsv);
+
+      FeModelLoader.LoadAndBuild(StrucCsv, PipeCsv, EquipCsv);
     }
 
   }
