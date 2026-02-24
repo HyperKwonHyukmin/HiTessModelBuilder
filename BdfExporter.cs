f064e3d89f5d109f66a14fef06ac9079f68223aa
HyperKwonHyukmin <khm7529@gmail.com>
2026-02-24 14:58:34 +0900

    Update BdfExporter.cs

--- BdfExporter.cs
+++ BdfExporter.cs
@@ -9,11 +9,11 @@ public static class BdfExporter
   public static void Export(
       FeModelContext context,
       string CsvPath,
-      string stageName)
+      string stageName, List<int> spcList = null)
   {
 
     // [수정] 계산된 maxLoadCaseID를 생성자에 전달
-    var bdfBuilder = new BdfBuilder(101, context);
+    var bdfBuilder = new BdfBuilder(101, context, spcList);
 
     bdfBuilder.Run();
     string newBdfName = stageName + ".bdf";
