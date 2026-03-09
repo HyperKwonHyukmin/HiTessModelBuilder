5e035eebc0e9f7d234cf286d0076f7287c861335
HyperKwonHyukmin <khm7529@gmail.com>
2026-03-09 11:30:42 +0900

    Update NodeDegreeInspector.cs

--- NodeDegreeInspector.cs
+++ NodeDegreeInspector.cs
@@ -1,17 +1,34 @@
 using HiTessModelBuilder.Model.Entities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class NodeDegreeInspector
 {
-  public static Dictionary<int, int> BuildNodeDegree(
-    FeModelContext context)
+  public static Dictionary<int, int> BuildNodeDegree(FeModelContext context)
   {
-    var degree = context.Nodes
-      .ToDictionary(kv => kv.Key, _ => 0);
+    var degree = context.Nodes.ToDictionary(kv => kv.Key, _ => 0);
 
-    foreach (var ele in context.Elements)
+    // 1. Elements (CBEAM 등 일반 부재) 카운트
+    foreach (var kvp in context.Elements)
     {
-      foreach (int nodeId in ele.Value.NodeIDs)
+      foreach (int nodeId in kvp.Value.NodeIDs)
+      {
+        if (degree.ContainsKey(nodeId))
+          degree[nodeId]++;
+      }
+    }
+
+    // 2. ★ Rigids (RBE) 카운트 추가
+    // 강체에 묶인 노드들도 위상학적으로 연결된 것으로 간주하여 차수(Degree)를 올립니다.
+    foreach (var kvp in context.Rigids)
+    {
+      var rbe = kvp.Value;
+      
+      if (degree.ContainsKey(rbe.IndependentNodeID))
+        degree[rbe.IndependentNodeID]++;
+
+      foreach (int nodeId in rbe.DependentNodeIDs)
       {
         if (degree.ContainsKey(nodeId))
           degree[nodeId]++;
@@ -21,4 +38,3 @@ public static class NodeDegreeInspector
     return degree;
   }
 }
-
